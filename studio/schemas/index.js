c471d375e77300604a0eebe6de9af563b8857a42
94tomas <nos18de@gmail.com>
2024-01-15 22:18:37 -0400

    add lead in studio

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -2,5 +2,12 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import lead from './lead'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [
+    post,
+    author,
+    category,
+    blockContent,
+    lead
+]
---
+++ studio/schemas/lead.js
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'lead',
+  title: 'Lead',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Nombre',
+      type: 'string',
+    }),
+  ],
+})
